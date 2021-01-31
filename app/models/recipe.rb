0cf9d99dbb2aeb7039d0f58f2b91eeb40276b84b
ajigherighe <akpoigherighe@gmail.com>
2021-01-31 16:26:23 -0500

    updated backend

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,85 +1,37 @@
 class Recipe
-  DB = PG.connect({:host=>"localhost", :port => 5432, :dbname => 'rails_react_recipe_development'})
+  DB = PG.connect({:host=>"", :port => 5432, :dbname => 'rails_react_recipe_development', :password => 'dbn3rd'})
 
-  def self.all
-      # results = DB.exec("SELECT * FROM recipes;")
-      results = DB.exec(<<-SQL
-        SELECT
-            recipes.*,
-            drinks.name,
-            drinks.ingredients,
-            drinks.instructions,
-            drinks.img_url
-        FROM recipes
-        LEFT JOIN drinks
-            ON recipes.recipe_id = drinks.id
-        ORDER BY recipes.id ASC
-    SQL
-    )
-      return results.each do |result|
-        if result ["recipe_id"]
-            recipe = {
-                    "id" => result["recipe_id"].to_i,
-                    "name" => result["name"],
-                    "ingredients" => result["ingredients"],
-                    "instructions" => result["instructions"],
-                    "img_url" => result["img_url"]
-                }
-      end
-        {
-            "id" => result["id"].to_i,
-            "name" => result["name"],
-            "ingredients" => result["ingredients"],
-            "instructions" => result["instructions"],
-            "img_url" => result["img_url"],
-            "recipe" => recipe
+    def self.all
+        # results = DB.exec("SELECT * FROM recipes;")
+        results = DB.exec("SELECT * FROM recipes;")
+        return results.each do |result|
+            {
+                "id" => result["recipe_id"].to_i,
+                "name" => result["name"],
+                "ingredients" => result["ingredients"],
+                "instructions" => result["instructions"],
+                "img_url" => result["img_url"]
+            }
+        end
+    end
+
+    def self.find(id)
+        results = DB.exec("SELECT * FROM recipes WHERE id=#{id};")
+        return {
+            "id" => results.first["id"].to_i,
+            "name" => results.first["name"],
+            "ingredients" => results.first["ingredients"],
+            "instructions" => results.first["instructions"],
+            "img_url" => results.first["img_url"],
         }
     end
-end
-
-  def self.find(id)
-    # results = DB.exec("SELECT * FROM recipes WHERE id=#{id};")
-    results = DB.exec(<<-SQL
-        SELECT
-            recipes.*,
-            drinks.name,
-            drinks.ingredients,
-            drinks.instructions,
-            drinks.img_url
-            FROM recipes
-        LEFT JOIN drinks
-            ON recipes.recipe_id = drinks.id
-        WHERE recipes.id = #{id};
-    SQL
-    )
-    result = results.first
-    if result ["recipe_id"]
-            recipe = {
-                    "id" => result["recipe_id"].to_i,
-                    "name" => result["name"],
-                    "ingredients" => result["ingredients"],
-                    "instructions" => result["instructions"],
-                    "img_url" => result["img_url"],
-                }
-    end
-
-    return {
-
-        "id" => result["id"].to_i,
-        "name" => result["name"],
-        "ingredients" => result["ingredients"],
-        "instructions" => result["instructions"],
-        "img_url" => result["img_url"],
-        "recipe" => recipe
-    }
-end
 
   def self.create(opts)
     results = DB.exec(
         <<-SQL
             INSERT INTO recipes (name, ingredients, instructions, img_url)
-            VALUES ( '#{opts["name"]}', '#{opts["ingredients"]}', '#{opts["instructions"]}', '#{opts["img_url"]}', #{opts["recipe_id"] ? opts["recipe_id"] : "NULL"})
-            RETURNING id, name, ingredients, instructions, img_url, recipe_id;
+            VALUES ( '#{opts["name"]}', '#{opts["ingredients"]}', '#{opts["instructions"]}', '#{opts["img_url"]}')
+            RETURNING id, name, ingredients, instructions, img_url;
         SQL
     )
     return {
@@ -88,7 +40,6 @@ end
         "ingredients" => results.first["ingredients"],
         "instructions" => results.first["instructions"],
         "img_url" => results.first["img_url"],
-        "recipe_id" => results.first["recipe_id"].to_i
     }
   end
 
@@ -101,14 +52,9 @@ end
     results = DB.exec(
         <<-SQL
             UPDATE recipes
-            SET
-            name='#{opts["name"]}',
-            ingredients='#{opts["ingredients"]}',
-            instructions='#{opts["instructions"]}',
-            img_url='#{opts["img_url"],}',
-            recipe_id=#{opts["recipe_id"] ? opts["recipe_id"] : NULL }
+            SET name='#{opts["name"]}', ingredients='#{opts["ingredients"]}', instructions='#{opts["instructions"]}', img_url='#{opts["img_url"]}'
             WHERE id=#{id}
-            RETURNING id, name,ingredients, instructions, img_url, recipe_id ;
+            RETURNING id, name, ingredients, instructions, img_url;
         SQL
     )
     return {
@@ -117,7 +63,6 @@ end
         "ingredients" => results.first["ingredients"],
         "instructions" => results.first["instructions"],
         "img_url" => results.first["img_url"],
-        "recipe_id" => results.first["recipe_id"].to_i
     }
   end
 end
